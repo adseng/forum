5596dd174ae0af7da03d0ed060a41692dd73d9de
huang <1983902539@qq.com>
2020-06-15 17:19:31 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+## javadn论坛
+## terminal 控制台 配置
+setting-->shell path -->D:\Program Files\git\Git\bin\bash.exe
---
+++ src/main/java/com/javadn/forum/controller/Hellocontroller.java
@@ -0,0 +1,18 @@
+package com.javadn.forum.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Hellocontroller {
+    @GetMapping("/hello")
+    public String hello(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+
+        return "hello";
+
+    }
+}
