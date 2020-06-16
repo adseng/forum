3a5520c8fcfa2a64243c1fde431c28ff39d502a1
huang <1983902539@qq.com>
2020-06-16 15:03:29 +0800

    add index and bootstrap

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 setting-->shell path -->D:\Program Files\git\Git\bin\bash.exe
 
 ## 使用https连接远程库
-config文件中
+config文件中修改url，这样不用每次提交都输入用户名和密码
     url = https://username:password@github.com/adseng/forum.git
+
 
--- src/main/java/com/javadn/forum/controller/IndexController.java
+++ src/main/java/com/javadn/forum/controller/IndexController.java
@@ -7,12 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class Hellocontroller {
-    @GetMapping("/hello")
-    public String hello(@RequestParam("name") String name, Model model){
-        model.addAttribute("name", name);
-
-        return "hello";
-
+public class IndexController {
+    @GetMapping("/")//匹配路经，/ 表示任何路经都不输入的时候默认访问的页面
+    public String index(){
+        return "index";
     }
 }
